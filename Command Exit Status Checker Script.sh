82964eb10c6b122854bfa24a6198575ceac98c0d
Audrey Washington-Wright <98628595+AudreyWash@users.noreply.github.com>
2022-07-30 20:37:33 -0500

    Update Command Exit Status Checker Script.sh

--- Command Exit Status Checker Script.sh
+++ Command Exit Status Checker Script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 package=htop
-sudo apt install $package
+sudo apt install $package >> package_install_results.log
 
 if [ $? -eq 0 ]
 then
     echo "The installation of $package was successful."
     echo "The new command is available here:"
 else
-    echo "$package failed to install."
+    echo "$package failed to install." >> package_install_failure.log
 fi
