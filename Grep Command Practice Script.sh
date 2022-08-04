488bdf80e777747029618195043951d47964ddf6
Audrey Washington-Wright <98628595+AudreyWash@users.noreply.github.com>
2022-08-03 21:58:53 -0500

    Update Grep Command Practice Script.sh

--- Grep Command Practice Script.sh
+++ Grep Command Practice Script.sh
@@ -20,3 +20,4 @@ fi
 # The text search pattern is called a regular expression. When it finds a match, it prints the line with the result. 
 # In this script, grep will be used to search the file $release_file (which is an actual file on Linux distributions) and will output whatever line the string
 # requested is on.
+# The script may not run if the Linux distribution is neither Arch or Ubuntu.
