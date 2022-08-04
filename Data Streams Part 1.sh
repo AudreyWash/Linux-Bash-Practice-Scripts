c89b98d2402e40eec46e4ec9ca96ad194aa99406
Audrey Washington-Wright <98628595+AudreyWash@users.noreply.github.com>
2022-08-04 18:13:49 -0500

    Update Data Streams Part 1.sh

--- Data Streams Part 1.sh
+++ Data Streams Part 1.sh
@@ -1,3 +1,11 @@
+find /etc -type f 1> /dev/null 
 find /etc -type f 2> /dev/null 
 
+# Notes:
+# 1 designates standard output
+# 2 designates standard error
+
+# /dev/null is a null device file. This will discard anything written to it, and will return EOF on reading. 
+# This is a command-line hack that acts as a vacuum, that sucks anything thrown to it.
+# The find command is used to search and locate the list of files and directories based on conditions you specify for files that match the arguments.
 
