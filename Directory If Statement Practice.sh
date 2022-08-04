c7807db2d318835303a4533712f8a883206def12
Audrey Washington-Wright <98628595+AudreyWash@users.noreply.github.com>
2022-08-03 21:45:40 -0500

    Update Directory If Statement Practice.sh

--- Directory If Statement Practice.sh
+++ Directory If Statement Practice.sh
@@ -2,7 +2,7 @@
 if [ -d /etc/pacman.d ]
 then 
     # The host is based on Arch, run the pacman update command
-    sudo pacman -Syu
+    sudo pacman -Syu 
 fi
 
 if [ -d /etc/apt ]
@@ -13,3 +13,11 @@ then
     sudo apt dist-upgrade
 fi
 
+# Notes:
+
+# -Syu is the command on Arch Linux systems that would be used to install all updates that are currently available. 
+# pacman.d is a directory commonly used on Arch Linux systems that manages software packages in Linux. 
+# [ -d /etc/apt ] is looking for a directory by the name of apt. apt is a file that is common on mostly Debian or Ubuntu distributions.
+
+# NOTE: The script will run based on what distribution of Linux you are using. If you are using Arch Linux, the if-statement for pacman.d will run, and
+# if on Debian or Ubuntu, then the if-statement for apt will run.
