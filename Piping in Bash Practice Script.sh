5dd7d0bf44d2654c8530540cd6061304387b2ea5
Audrey Washington-Wright <98628595+AudreyWash@users.noreply.github.com>
2022-08-06 20:20:24 -0500

    Update Piping in Bash Practice Script.sh

--- Piping in Bash Practice Script.sh
+++ Piping in Bash Practice Script.sh
@@ -6,7 +6,7 @@ then
     sudo pacman -Syu
 fi
 
-if grep -q "Debian" $release_file || grep -q "Ubuntu" $release_file  # The || symbol stands for "or."
+if grep -q "Debian" $release_file || grep -q "Ubuntu" $release_file  # The || symbol stands for "or" which is piping in Linux.
 then
     # The host is based on Debian or Ubuntu,
     # Run the apt version of the command
