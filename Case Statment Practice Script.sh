50adaa1fb038cf7dfd690f0e6618706060c78b70
Audrey Washington-Wright <98628595+AudreyWash@users.noreply.github.com>
2022-08-07 19:41:06 -0500

    Update Case Statment Practice Script.sh

--- Case Statment Practice Script.sh
+++ Case Statment Practice Script.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-echo "What is your favorite Linux distribution?"
+read finished
 
-echo "1 - Arch"
-echo "2 - CentOS"
-echo "3 - Debian"
-echo "4 - Mint"
-echo "5 - Ubuntu"
-echo "6 - Something else"
+while [ $finished -ne 1 ]
+do
+  echo "What is your favorite Linux distribution?"
+  echo "1 - Arch"
+  echo "2 - CentOS"
+  echo "3 - Debian"
+  echo "4 - Mint"
+  echo "5 - Ubuntu"
+  echo "6 - Something else..."
+  echo "7 - Exit the script."
 
-# Standard input command
-read distribution;
+  # Standard input command
+  read distribution;
 
-# Making case statments
-case $distribution in
-    1) echo "Arch is rolling on release.";;
-    2) echo "CentOS is popular on servers.";;
-    3) echo "Debian is a community distribution";;
-    4) echo "Mint is popular on desktops and laptops.";;
-    5) echo "Ubuntu is popular on both servers and computers.";;
-    6) echo "There are many distributions out there.";;
-    *) echo "You didn't enter an appropriate choice.";;
-esac
+  case $distribution in
+      1) echo "Arch is rolling on release.";;
+      2) echo "CentOS is popular on servers.";;
+      3) echo "Debian is a community distribution";;
+      4) echo "Mint is popular on desktops and laptops.";;
+      5) echo "Ubuntu is popular on both servers and computers.";;
+      6) echo "There are many distributions out there.";;
+      7) finished=1 ;;
+      *) echo "You didn't enter an appropriate choice.";;
+  esac
+done
